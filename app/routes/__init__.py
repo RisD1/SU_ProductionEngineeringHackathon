2eeb71d1ac088b448625c676b68421609a722d61
Rishika Das <68506192+RisD1@users.noreply.github.com>
2026-04-05 14:11:25 +0530

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,10 @@
 def register_routes(app):
-    from app.routes.shortener import shortener_bp
+    
     from app.routes.users import users_bp
     from app.routes.url import url_bp
     from app.routes.events import events_bp
 
-    app.register_blueprint(shortener_bp)
+   
     app.register_blueprint(events_bp)
     app.register_blueprint(url_bp)
     app.register_blueprint(users_bp)
